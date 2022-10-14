1cdb1f70a31a52174954fba406def3a5a7f7087e
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2022-10-14 15:40:26 +0300

    Generate project template.

---
+++ examples/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <cstdlib>
+
+auto main(int argc, const char* argv[]) -> int
+{
+    (void)fprintf(stdout, "%s", "Welcome to the image processing libarary. \nLet see under examples to use it\n");
+    return EXIT_SUCCESS;
+}
